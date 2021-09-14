37e1c82d25bde6f37a1345d800a8ad788c1ccb41
Alvin-Voo <alvin.voo@gmail.com>
2021-09-14 22:49:37 +0800

    realized there are 3 components : | and space, and there's premature splitting at line 29

--- 192020/monster_message/lib/monster_message.ex
+++ 192020/monster_message/lib/monster_message.ex
@@ -22,17 +22,17 @@ defmodule MonsterMessage do
     value = node_map[node_index]
     IO.puts inspect "node index " <> node_index <> " value " <> value
     if value=="a" || value=="b" do
-      [value]
+      value
     else
       if Regex.match?(~r/\|/, value) do
         [left_tree, right_tree] = String.split(value, "|", trim: true)
+        # seems premature to split it here first..
+        # all the | are OR while space " " are AND
         left_nodes = String.split(String.trim(left_tree), " ", trim: true)
         right_nodes = String.split(String.trim(right_tree), " ", trim: true)
 
         left_message = Enum.reduce(left_nodes, [], fn node, acc ->
-          acc = acc ++ recurse_number(node_map, node)
-          IO.puts "acc L msg " <> inspect acc
-          acc
+          acc ++ [recurse_number(node_map, node)]
         end)
 
         left_message = if Enum.all?(left_message, fn x -> x == "a" || x == "b" end) do
@@ -40,11 +40,10 @@ defmodule MonsterMessage do
         else
           left_message
         end
+        IO.puts "L msg " <> inspect left_message
 
         right_message = Enum.reduce(right_nodes, [], fn node, acc ->
-          acc = acc ++ recurse_number(node_map, node)
-          IO.puts "acc R msg " <> inspect acc
-          acc
+          acc ++ [recurse_number(node_map, node)]
         end)
 
         right_message = if Enum.all?(right_message, fn x -> x == "a" || x == "b" end) do
@@ -52,22 +51,33 @@ defmodule MonsterMessage do
         else
           right_message
         end
+        IO.puts "R msg " <> inspect right_message
 
-        [[left_message, right_message]]
+        [left_message, right_message]
       else
         nodes = String.split(String.trim(value), " ", trim: true)
         IO.puts inspect "Nodes " <> inspect nodes
         Enum.reduce(nodes, [] , fn node, acc ->
-          acc ++ recurse_number(node_map, node)
+          temp = [recurse_number(node_map, node)]
+          IO.puts "ACC & #{inspect acc} #{inspect temp}"
+          acc ++ temp
         end)
-        # need to be able to join here
       end
     end
   end
 
-
+  # && function
   # given 2 lists, return the whole permutation range
   def permute_list(list1, list2) do
+    Enum.reduce(list1, [], fn item, acc ->
+      item_list = for r <- list2 do
+        item <> r
+      end
+      acc ++ item_list
+    end)
+  end
+
+  def permute_list2(list1, list2) do
     Enum.reduce(list1, [], fn item, acc ->
       item_list = for r <- list2 do
         item <> r
